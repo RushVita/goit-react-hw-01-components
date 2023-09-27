a1bd2ccc4f464c1e1f022ce3523675d83c791dbe
RushVita <129496341+RushVita@users.noreply.github.com>
2023-09-27 03:10:59 +0300

    js.done

---
+++ src/components/Statistics.js
@@ -0,0 +1,17 @@
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className="statistics">
+      <h2 className="title">{title}</h2>
+      <ul className="stat-list">
+        {stats.map(item => {
+          return (
+            <li key={item.id}>
+              <span className="label">{item.label}</span>
+              <span className="percentage">{item.percentage}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
